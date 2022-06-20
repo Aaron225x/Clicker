5e6b6eb7bdafd28dd527d2ba1de400e39e2e6a9f
nefir2 <vadim.ahtamov@gmail.com>
2022-06-20 12:50:53 +0300

    Added class for saving any settings in program

---
+++ Кликер/Classes/Saver.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Кликер;
+
+namespace Кликер.Classes
+{
+	class Saver
+	{
+		public static void SaveAll()
+		{
+			Form1 main = (Form1)GetForm("Form1");
+			if (main == null) { } //затычка на случай если нужная форма окажется закрыта.
+			//main.Money
+		}
+		public static Form GetForm(string name)
+        {
+			Form getter = null;
+			for (int i = 0; i < Application.OpenForms.Count; i++) //проход по всем открытм формам.
+			{
+				if (Application.OpenForms[i].Name == name) //если нашли нужную открытую форму
+				{
+					getter = (Form1)Application.OpenForms[i]; //сохраняем в отдельную переменную.
+					break; //сворачиваем выполнение цикла от лишних нагрузок.
+				}
+			}
+			return getter;
+		}
+	}
+}
