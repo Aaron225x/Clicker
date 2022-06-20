57c85444ba3a1644c1658861aab24d5e84aa4473
nefir2 <vadim.ahtamov@gmail.com>
2022-06-20 12:17:06 +0300

    if found, end method.

--- Кликер/Achievements.cs
+++ Кликер/Achievements.cs
@@ -10,34 +10,37 @@ using System.Windows.Forms;
 
 namespace Кликер
 {
-    public partial class Achievements : Form
-    {
-        public Achievements()
-        {
-            InitializeComponent();
-        }
-        /// <summary>
-        /// Закрывает все формы
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void ClosingForms(object sender, FormClosingEventArgs e)
-        {
-            Application.Exit();
-        }
-        /// <summary>
-        /// Кнопка перехода в форму кликер
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void TransitionMain(object sender, EventArgs e)
-        {
-            Hide();
-            for (int i = 0; i < Application.OpenForms.Count; i++) //проход по всем открытм формам.
-            {
-                if (Application.OpenForms[i].Name == "Form1") //если нашли нужную открытую форму
-                    Application.OpenForms[i].Show(); //показываем её.
-            }
-        }
-    }
+	public partial class Achievements : Form
+	{
+		public Achievements()
+		{
+			InitializeComponent();
+		}
+		/// <summary>
+		/// Закрывает все формы
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void ClosingForms(object sender, FormClosingEventArgs e)
+		{
+			Application.Exit();
+		}
+		/// <summary>
+		/// Кнопка перехода в форму кликер
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void TransitionMain(object sender, EventArgs e)
+		{
+			Hide();
+			for (int i = 0; i < Application.OpenForms.Count; i++) //проход по всем открытм формам.
+			{
+				if (Application.OpenForms[i].Name == "Form1") //если нашли нужную открытую форму
+				{
+					Application.OpenForms[i].Show(); //показываем её.
+					return; //сворачиваем выполнение метода от лишних нагрузок.
+				}
+			}
+		}
+	}
 }
