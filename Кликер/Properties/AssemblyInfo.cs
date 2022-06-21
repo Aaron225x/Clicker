25c9bc2ceb9275c2946b2fac44a3a6f31fb9c8b7
nefir2 <vadim.ahtamov@gmail.com>
2022-06-21 08:11:30 +0300

    я так понял в этом файле можно изменять инфу программы.

--- Кликер/Properties/AssemblyInfo.cs
+++ Кликер/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Кликер")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("TeaTeam")]
 [assembly: AssemblyProduct("Кликер")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
